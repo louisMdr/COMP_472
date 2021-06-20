2d0444fd594673922a22885c696d740bb7e9ee9a
louisMdr <madrelouis1@gmail.com>
2021-06-20 15:35:40 -0400

    Modified graph x labels, added 0 model & fix >= 9

--- nlp/pipeline/word_length.py
+++ nlp/pipeline/word_length.py
@@ -7,7 +7,7 @@ from nlp.pipeline.bayes import NaiveBayesClassifier
 
 class WordLengthClassifier(NaiveBayesClassifier):
     # the word with the following length to be removed from training data
-    word_length_cap = [2, 4, 9]
+    word_length_cap = [0, 2, 4, 9]
 
     # keeps track of which length of words to remove in each model preparation
     iteration: int
@@ -23,8 +23,13 @@ class WordLengthClassifier(NaiveBayesClassifier):
         super().prepare(training_data)
         # copy list of X (list of reviews in the normal training data)
         new_training = self.training_data.copy()
-        # checks if we are getting words > 2, 4 or words < 9 (last iteration)
-        if self.iteration == 3:
+
+        # checks if we are getting words = 0, (> 2 or 4) or words < 9 (last iteration)
+        if self.iteration == 0:
+          self.training_data = new_training
+        elif self.iteration == 3:
+            #since the assumption is gradual = cumulative, length must also be > 4 (= between 5 and 8 inclusive)
+            new_training = new_training[new_training['length'] > self.word_length_cap[self.iteration-1]]
             new_training = new_training[new_training['length'] < self.word_length_cap[self.iteration]]
         else:
             new_training = new_training[new_training['length'] > self.word_length_cap[self.iteration]]
@@ -43,12 +48,17 @@ class WordLengthIterator:
         self.y = y
 
     def iterate(self):
+        #accuracy results
         accuracy_results = []
+        #stores the word counts remaining in each iteration
+        word_count_results = []
         print('\nStarting Task 2.3 - Word Length Filtering...\n')
         wlc = WordLengthClassifier()
         for element in WordLengthClassifier.word_length_cap:
             # print correct brackets
-            if element == 9:
+            if element == 0:
+                print(f'  LENGTH = {element}')
+            elif element == 9:
                 print(f'  LENGTH <= {element}')
             else:
                 print(f'  LENGTH >= {element}')
@@ -65,10 +75,14 @@ class WordLengthIterator:
             print(f'    Accuracy: {res}')
             # add new accuracy to list
             accuracy_results.append(res)
+            #get vocab words count remaining
+            word_count_results.append(wlc.training_data.shape[0])
         print('\nTask 2.3 complete. Graph: \n')
+        #the amount of words removed
+        keys = ["=0", "<=2", "<=4", ">=9"]
         # performance (accuracy here) against nbr words left ( = word lengths removed)
-        plt.plot(["<=2", "<=4", ">=9"], [i for i in accuracy_results])
+        plt.plot([f'{keys[i]}: {word_count_results[i]}' for i in len(word_count_results)], [i for i in accuracy_results])
         plt.title('Naives Bayes Classification with Word Length (2.3)')
         plt.ylabel('Accuracy')
-        plt.xlabel('Removed Words Lengths')
+        plt.xlabel('Words Left In Vocab')
         plt.show()
